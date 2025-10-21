594e16010a6e79abb5a87936582bb79290c68348
Gokul <gokul.m@inapp.com>
2025-10-21 20:01:41 +0530

    Refactor ad routes and enhance ad fetching functionality

--- routes/ad.routes.js
+++ routes/ad.routes.js
@@ -1,14 +1,15 @@
 import express from "express";
-import { createAd, getAllAds, getAdById, deleteAd,getAllAdCategories } from "../controllers/ad.controller.js";
+import { createAd, getAllAds, getAdById, deleteAd,getAllAdCategories,getUserAds } from "../controllers/ad.controller.js";
 import authMiddleware from "../middleware/auth.js";
 import fileUpload from "../middleware/fileUpload.js";
 
 const router = express.Router();
 
-router.post("/postAdd", authMiddleware, fileUpload.array('image'), createAd);
-router.get("/", getAllAds);
 router.get("/listCategories", getAllAdCategories);
-router.delete("/:id", authMiddleware, deleteAd);
+router.post("/listUserAds", getUserAds);
+router.post("/", getAllAds);
+router.post("/postAdd", authMiddleware, fileUpload.array('image'), createAd);
 router.get("/:id", getAdById);
+router.delete("/:id", authMiddleware, deleteAd);
 
 export default router;
