a34cf9a86ae7b164885df0ef3ec9a19fbefe0c68
Gokul <gokul.m@inapp.com>
2025-10-28 22:04:22 +0530

    Implement editAd functionality and update ad routes; add image upload support

--- routes/ad.routes.js
+++ routes/ad.routes.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { createAd, getAllAds, getAdById, deleteAd,getAllAdCategories,getUserAds } from "../controllers/ad.controller.js";
+import { createAd, getAllAds, getAdById, deleteAd,getAllAdCategories,getUserAds,editAd } from "../controllers/ad.controller.js";
 import authMiddleware from "../middleware/auth.js";
 import fileUpload from "../middleware/fileUpload.js";
 
@@ -11,5 +11,5 @@ router.post("/", getAllAds);
 router.post("/postAdd", authMiddleware, fileUpload.array('image'), createAd);
 router.get("/:id", getAdById);
 router.delete("/:id", authMiddleware, deleteAd);
-
+router.put("/edit/:id", authMiddleware, fileUpload.array('image'), editAd);
 export default router;
